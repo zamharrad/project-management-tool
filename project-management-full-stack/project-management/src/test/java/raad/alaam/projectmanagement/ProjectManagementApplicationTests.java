6d9d718766c3b9f22ee55770fd2b5ab64cdbf97c
Zamhar Raa'D <41635859+zamharrad@users.noreply.github.com>
2020-03-01 23:25:29 +0530

    add spring boot project with basic structure

---
+++ project-management-full-stack/project-management/src/test/java/raad/alaam/projectmanagement/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package raad.alaam.projectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
