b45c351123ea84e2c5c07238e88afe3f059c7467
zamharrad <mhmdzamhar@gamil.com>
2020-03-05 23:53:15 +0530

    commit project validation and project controller

--- project-management-full-stack/project-management/src/main/java/raad/alaam/projectmanagement/controllers/ProjectController.java
+++ project-management-full-stack/project-management/src/main/java/raad/alaam/projectmanagement/controllers/ProjectController.java
@@ -3,9 +3,15 @@ package raad.alaam.projectmanagement.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import raad.alaam.projectmanagement.domain.Project;
 import raad.alaam.projectmanagement.services.ProjectService;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -15,8 +21,17 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
+        if (result.hasErrors()){
+
+            Map<String,String> errorMap = new HashMap<>();
+
+            for(FieldError error : result.getFieldErrors()){
+                    errorMap.put(error.getField(),error.getDefaultMessage());
+            }
+            return new ResponseEntity<>(errorMap,HttpStatus.BAD_REQUEST);
+        }
         Project project1 = projectService.saveAndUpdateProject(project);
         return new ResponseEntity<Project>(project,HttpStatus.CREATED);
     }
