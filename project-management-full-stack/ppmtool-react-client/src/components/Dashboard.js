64461d0eb5d3390abc307dbe81595d4fdfebfb09
zamharrad <mhmdzamhar@gamil.com>
2020-03-23 21:03:23 +0530

    commit UI changes

--- project-management-full-stack/ppmtool-react-client/src/components/Dashboard.js
+++ project-management-full-stack/ppmtool-react-client/src/components/Dashboard.js
@@ -6,11 +6,11 @@ class Dashboard extends Component {
   render() {
     return (
       <div>
-        <div class="projects">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-12">
-                <h1 class="display-4 text-center">Projects</h1>
+        <div className="projects">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 className="display-4 text-center">Projects</h1>
                 <br />
                 <CreateProjectButton />
                 <br />
