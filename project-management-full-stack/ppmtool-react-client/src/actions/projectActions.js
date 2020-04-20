1779ba310ad39fdafaddcd101170092e50e2281e
zamharrad <mhmdzamhar@gamil.com>
2020-04-20 06:37:38 +0530

    commit project props changes in add project file

--- project-management-full-stack/ppmtool-react-client/src/actions/projectActions.js
+++ project-management-full-stack/ppmtool-react-client/src/actions/projectActions.js
@@ -1,20 +1,28 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_PROJECTS } from "./types";
 
-export const createProject = (project, history) => async dispatch => {
+export const createProject = (project, history) => async (dispatch) => {
   try {
     const res = await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
-  } catch (err) {  
+  } catch (err) {
     dispatch({
       type: GET_ERRORS,
-      payload: err.response.data
+      payload: err.response.data,
     });
   }
 };
 
-export const updateProject = (project, history) => async dispatch => {
-  try {
-    const res = await axios.post("http://localhost:8080/api/project", project);
-  } catch (error) {}
+// export const updateProject = (project, history) => async (dispatch) => {
+//   try {
+//     const res = await axios.post("http://localhost:8080/api/project", project);
+//   } catch (error) {}
+// };
+
+export const getProject = () => async (dispatch) => {
+  const res = await axios.get("http://localhost:8080/api/project/all");
+  dispatch({
+    type: GET_PROJECTS,
+    payload: res.data,
+  });
 };
