d64e7f36aa894e58d97280e6cbd16f4bcedce40e
zamharrad <mhmdzamhar@gamil.com>
2020-09-14 11:27:57 +0530

    update Project domain file

--- project-management-full-stack/project-management/src/main/java/raad/alaam/projectmanagement/domain/project.java
+++ project-management-full-stack/project-management/src/main/java/raad/alaam/projectmanagement/domain/project.java
@@ -30,7 +30,7 @@ public class Project {
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
     @JsonFormat(pattern = "yyyy-mm-dd")
-    @Column(updatable = false)
+	@Column(updatable = false)
     private Date created_At;
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
